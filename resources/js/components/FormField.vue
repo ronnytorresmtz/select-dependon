3f0e91a0d588f51fd886cde4225be94a04238847
Rolando Torres <ronnytorresmtz@gmail.com>
2019-06-01 22:46:34 -0500

    first commit

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -1,47 +1,107 @@
 <template>
     <default-field :field="field" :errors="errors">
         <template slot="field">
-            <input
-                :id="field.name"
-                type="text"
-                class="w-full form-control form-input form-input-bordered"
-                :class="errorClasses"
-                :placeholder="field.name"
+            <select-control
+                :id="field.attribute"
+                :dusk="field.attribute"
                 v-model="value"
-            />
+                class="w-full form-control form-select"
+                :class="errorClasses"
+                :options="field.options"
+                :disabled="isReadonly"
+                @change.prevent="onClick(value)"
+            >
+                <option value="" selected>{{ __('Choose an option') }}</option>
+            </select-control>
         </template>
     </default-field>
 </template>
 
 <script>
 import { FormField, HandlesValidationErrors } from 'laravel-nova'
-
 export default {
-    mixins: [FormField, HandlesValidationErrors],
-
-    props: ['resourceName', 'resourceId', 'field'],
-
+    mixins: [HandlesValidationErrors, FormField],
+    mounted() {
+        this.addFieldInfoToStore();
+        const store = Nova.store.getters.getData
+        const fieldAttribute = this.field.attribute;
+        const fieldValue = this.field.value;
+        let optionsToAdd = {};
+        Object.keys(store).forEach(function(idx) {
+            if (store[idx].dependOn !== undefined) {
+                if (store[idx].field.toLowerCase() === fieldAttribute) {
+                    const dependOn = store[idx].dependOn; 
+                    if (store[idx].dependOn !== undefined) {
+                        Object.keys(store).forEach(function(idx) {
+                            if (store[idx].field === dependOn.toLowerCase()) {
+                                const value = store[idx].value;
+                                Object.keys(store).forEach(function(idx) {
+                                    if (store[idx].dependOn !== undefined) {
+                                        if (store[idx].dependOn.toLowerCase() === dependOn.toLowerCase()) {
+                                            optionsToAdd = {
+                                                'options': store[idx].options[value],
+                                                'field': store[idx].field,
+                                            };
+                                        }
+                                    }
+                                });
+                            }
+                        });
+                    }
+                                   
+                }
+            }
+        });
+        this.addOptionsToSelect(optionsToAdd);
+    },
     methods: {
-        /*
-         * Set the initial, internal value for the field.
-         */
-        setInitialValue() {
-            this.value = this.field.value || ''
-        },
-
         /**
-         * Fill the given FormData object with the field's internal value.
+         * Provide a function that fills a passed FormData object with the
+         * field's internal value attribute. Here we are forcing there to be a
+         * value sent to the server instead of the default behavior of
+         * `this.value || ''` to avoid loose-comparison issues if the keys
+         * are truthy or falsey
          */
         fill(formData) {
-            formData.append(this.field.attribute, this.value || '')
+            formData.append(this.field.attribute, this.value)
         },
-
-        /**
-         * Update the field's internal value.
-         */
-        handleChange(value) {
-            this.value = value
+        onClick(value) {
+            this.addFieldInfoToStore();
+            const fieldAttribute = this.field.attribute;
+            const store = Nova.store.getters.getData
+            let optionsToAdd = {};
+            Object.keys(store).forEach(function(idx) {
+                if (store[idx].dependOn !== undefined) {
+                    if (store[idx].dependOn.toLowerCase() == fieldAttribute) {
+                        optionsToAdd = {
+                           'options': store[idx].options[value],
+                           'field': store[idx].field,
+                        };
+                    }
+                }
+            });
+            this.addOptionsToSelect(optionsToAdd);
         },
+        addOptionsToSelect(optionsToAdd) {
+            if (! (_.isEmpty(optionsToAdd))) {
+                document.getElementById(optionsToAdd.field).options.length = 1;
+                var el = document.getElementById(optionsToAdd.field);
+                for(var i in optionsToAdd.options) {
+                    var option = document.createElement("option");
+                    option.text = optionsToAdd.options[i];
+                    option.label = optionsToAdd.options[i];
+                    el.add(option);
+                }
+            }
+        },
+        addFieldInfoToStore() {
+            Nova.store.dispatch('addSelectOption', {
+                'field': this.field.attribute, 
+                'value': this.field.value,
+                'dependOn': this.field.dependOn,
+                'options': this.field.dependValues,
+            });
+        }
     },
 }
 </script>
--- resources/js/field.js
+++ resources/js/field.js
@@ -1,5 +1,39 @@
+import Vuex from 'vuex';
+
 Nova.booting((Vue, router, store) => {
     Vue.component('index-select-depend-on', require('./components/IndexField'))
     Vue.component('detail-select-depend-on', require('./components/DetailField'))
     Vue.component('form-select-depend-on', require('./components/FormField'))
+
+    Vue.use(Vuex);
+
+    Nova.store = new Vuex.Store({
+        state: {
+            data: []
+        },
+        mutations: {
+            ADD_SELECT_OPTION(state, data) {
+                let bExist = false;
+                state.data.forEach(function(item, index) {
+                    if (item.field == data.field) {
+                        item.value = data.value;
+                        bExist = true;
+                    }
+                });
+                if (! bExist) {
+                    state.data.push(data);
+                }
+            }
+        },
+
+        actions: {
+            addSelectOption(context, data) {
+                context.commit('ADD_SELECT_OPTION', data);
+              },
+
+        },
+        getters: {
+            getData: state => state.data,
+        },
+    });
 })
